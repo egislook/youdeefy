703ce0c4c5aff4752ed9b80bd4d34a097391351c
Egidijus <noneedsystem@gmail.com>
2019-05-25 12:30:01 +0000

    log the start playing track

--- src/@youdeefy/components/player/collectors.js
+++ src/@youdeefy/components/player/collectors.js
@@ -3,42 +3,13 @@ const HASURA_URL = 'https://youdeespot.com/v1alpha1/graphql';
 
 const API_URL = true ? 'https://youdeespot.com' : 'https://pwa-noneede.c9users.io:8081';
 
-const queries = {
-  tracks: `query($videoIds: [String]) { track(where: { videoId: { _in: $videoIds } }) { id title name artist img videoId altVideoId } }`,
-  playlists: `query { playlist (order_by: {createdAt: desc}, limit: 50) { id title playlistId title description img channelTitle itemCount } }`,
-  playlist: (playlistId) => `query { playlist(where: { playlistId: { _eq: "${playlistId}"}}) { id title playlistId title description img channelTitle itemCount videoIds } }`
-}
-
-export function fetchPlaylists() {
-  return GQL({ url: HASURA_URL, query: queries.playlists })
-    .then(data => data.playlist)
-}
-
-export function importPlaylist(playlistId){
-  return POST({ url: API_URL + '/api/import', body: { playlistId } })
-}
-
 export function fetchTrack(body) {
   
   return POST({ url: API_URL + '/api/url', body })
-    .then(res => res.data)
-    .then(data => {
-      console.log(data);
-      const format = data && data.formats && data.formats.find( f => f.itag === '140') || {};
+    // .then(res => res.data)
+    .then(res => {
+      console.log(res);
+      const format = res && res.data && res.data.formats && res.data.formats.find( f => f.itag === '140') || {};
       return format.url;
     });
-}
-
-export async function fetchPlaylist(playlistId){
-  
-  const playlist = await GQL({ url: HASURA_URL, query: queries.playlist(playlistId)})
-    .then(({ playlist: [playlist] }) => playlist);
-    
-  const { videoIds } = playlist;
-  
-  return GQL({ url: HASURA_URL, query: queries.tracks, variables: { videoIds }  })
-    .then(data => {
-      const tracks = videoIds.map( videoId => data.track.find(track => track.videoId === videoId) )
-      return ({ ...playlist, tracks })
-    })
 }
